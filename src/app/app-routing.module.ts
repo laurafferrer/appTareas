625af4016987f199bb36f0893a1ad5603b64251f
Laura <lferrere.ausiasmarch@gmail.com>
2023-11-14 10:35:54 +0100

    --

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //--
 import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
+import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
+import { LogoutRoutedComponent } from './components/shared/logout-routed/logout-routed.component';
 //--
 import { AdminUsuarioViewRoutedComponent } from './components/usuario/admin-usuario-view-routed/admin-usuario-view-routed.component';
 import { AdminUsuarioPlistRoutedComponent } from './components/usuario/admin-usuario-plist-routed/admin-usuario-plist-routed.component';
 import { AdminUsuarioEditRoutedComponent } from './components/usuario/admin-usuario-edit-routed/admin-usuario-edit-routed.component';
 import { AdminUsuarioNewRoutedComponent } from './components/usuario/admin-usuario-new-routed/admin-usuario-new-routed.component';
-import { AdminThreadNewRoutedComponent } from './components/thread/admin-thread-new-routed/admin-thread-new-routed.component';
-import { AdminTareaPlistRoutedComponent } from './components/tarea/admin-tarea-plist-routed/admin-tarea-plist-routed.component';
-import { AdminThreadPlistRoutedComponent } from './components/thread/admin-thread-plist-routed/admin-thread-plist-routed.component';
-import { AdminTareaNewRoutedComponent } from './components/tarea/admin-tarea-new-routed/admin-tarea-new-routed.component';
 //--
+import { AdminProyectoNewRoutedComponent } from './components/proyecto/admin-proyecto-new-routed/admin-proyecto-new-routed.component';
+import { AdminProyectoPlistRoutedComponent } from './components/proyecto/admin-proyecto-plist-routed/admin-proyecto-plist-routed.component';
+import { AdminProyectoEditRoutedComponent } from './components/proyecto/admin-proyecto-edit-routed/admin-proyecto-edit-routed.component';
+import { AdminProyectoViewRoutedComponent } from './components/proyecto/admin-proyecto-view-routed/admin-proyecto-view-routed.component';
+//--
+import { AdminTareaPlistRoutedComponent } from './components/tarea/admin-tarea-plist-routed/admin-tarea-plist-routed.component';
+import { AdminTareaNewRoutedComponent } from './components/tarea/admin-tarea-new-routed/admin-tarea-new-routed.component';
 import { AdminTareaEditRoutedComponent } from './components/tarea/admin-tarea-edit-routed/admin-tarea-edit-routed.component';
-import { AdminThreadEditRoutedComponent } from './components/thread/admin-thread-edit-routed/admin-thread-edit-routed.component';
-import { AdminThreadViewRoutedComponent } from './components/thread/admin-thread-view-routed/admin-thread-view-routed.component';
-import { AdminViewRoutedComponent } from './components/tarea/admin-tarea-view-routed/admin-tarea-view-routed.component';
-import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
-import { LogoutRoutedComponent } from './components/shared/logout-routed/logout-routed.component';
-
+import { AdminTareaViewRoutedComponent } from './components/tarea/admin-tarea-view-routed/admin-tarea-view-routed.component';
 
 const routes: Routes = [
   { path: '', component: HomeRoutedComponent },
@@ -32,15 +32,15 @@ const routes: Routes = [
   { path: 'admin/usuario/new', component: AdminUsuarioNewRoutedComponent },
   { path: 'admin/usuario/edit/:id', component: AdminUsuarioEditRoutedComponent },
   //--  
-  { path: 'admin/thread/plist', component: AdminThreadPlistRoutedComponent },
-  { path: 'admin/thread/plist/byuser/:id', component: AdminThreadPlistRoutedComponent },
-  { path: 'admin/thread/view/:id', component: AdminThreadViewRoutedComponent },    
-  { path: 'admin/thread/new', component: AdminThreadNewRoutedComponent },  
-  { path: 'admin/thread/edit/:id', component: AdminThreadEditRoutedComponent },  
+  { path: 'admin/proyecto/plist', component: AdminProyectoPlistRoutedComponent },
+  { path: 'admin/proyecto/plist/byuser/:id', component: AdminProyectoPlistRoutedComponent },
+  { path: 'admin/proyecto/view/:id', component: AdminProyectoViewRoutedComponent },    
+  { path: 'admin/proyecto/new', component: AdminProyectoNewRoutedComponent },  
+  { path: 'admin/proyecto/edit/:id', component: AdminProyectoEditRoutedComponent },  
   //--
   { path: 'admin/tarea/plist', component: AdminTareaPlistRoutedComponent },
   { path: 'admin/tarea/plist/byuser/:iduser', component: AdminTareaPlistRoutedComponent },  
-  { path: 'admin/tarea/plist/bythread/:idthread', component: AdminTareaPlistRoutedComponent },  
+  { path: 'admin/tarea/plist/byproyecto/:idproyecto', component: AdminTareaPlistRoutedComponent },  
   { path: 'admin/tarea/view/:id', component: AdminTareaViewRoutedComponent },    
   { path: 'admin/tarea/new', component: AdminTareaNewRoutedComponent},  
   { path: 'admin/tarea/edit/:id', component: AdminTareaEditRoutedComponent },
