965a686d7f2470bcc61cf99988fa84905413d087
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2023-11-21 17:48:55 +0100

    modificacion index y environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -10,9 +10,9 @@ export const httpOptions = {
 
 export const CALENDAR_ES = {
     firstDayOfWeek: 1,
-    dyNames: ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'],
-    dyNamesShort: ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom'],
-    dyNamesMin: ['L', 'M', 'X', 'J', 'V', 'S', 'D'],
+    dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+    dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+    dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
     monthNames: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
     monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
     today: 'Hoy',
